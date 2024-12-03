0843c5c74561fa387e0c53d8c69690bbbb1bc477
Kash1f <ahmed.kashif1001@gmail.com>
2024-12-03 20:58:03 +0500

    pages, react router dom setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import Home from "./pages/Home/Home"
+import Users from "./pages/Users/Users"
+import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom"
 
 const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/users",
+      element: <Users/>,
+    }
+  ])
+
   return (
-    <div>
-      <Home/>
-    </div>
+  <RouterProvider router={router} />
   )
 }
 
