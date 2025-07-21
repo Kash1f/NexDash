4195f3486c61a30c821978134e69dd95547f702e
Kashif <ahmed.kashif1001@gmail.com>
2025-07-21 15:25:13 +0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,52 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-} from "react-router-dom";
 import Users from "./pages/Users/Users";
 import Products from "./pages/Products/Products";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Menu from "./components/Menu/Menu";
 import Login from "./pages/Login/Login";
-import "./styles/global.scss";
 import User from "./pages/User/User";
 import Product from "./pages/Product/Product";
+import "./styles/global.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-function App() {
-  const Layout = () => {
-    return (
-      <div className="main">
-        <Navbar />
-        <div className="container">
-          <div className="menuContainer">
-            <Menu />
-          </div>
-          <div className="contentContainer">
-            <QueryClientProvider client={queryClient}>
-              <Outlet />
-            </QueryClientProvider>
-          </div>
+function Layout() {
+  return (
+    <div className="main">
+      <Navbar />
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
         </div>
-        <Footer />
+        <div className="contentContainer">
+          <QueryClientProvider client={queryClient}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="users" element={<Users />} />
+              <Route path="users/:id" element={<User />} />
+              <Route path="products" element={<Products />} />
+              <Route path="products/:id" element={<Product />} />
+            </Routes>
+          </QueryClientProvider>
+        </div>
       </div>
-    );
-  };
+      <Footer />
+    </div>
+  );
+}
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "",
-          element: <Home />,
-        },
-        {
-          path: "users",
-          element: <Users />,
-        },
-        {
-          path: "products",
-          element: <Products />,
-        },
-        {
-          path: "users/:id",
-          element: <User />,
-        },
-        {
-          path: "products/:id",
-          element: <Product />,
-        },
-      ],
-    },
-    {
-      path: "login",
-      element: <Login />,
-    },
-  ],
-   {
-    basename: "/NexDash",
-  }
-);
-
-  return <RouterProvider router={router} />;
+function App() {
+  return (
+    <BrowserRouter basename="/NexDash">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Layout />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
