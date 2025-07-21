b74b03b7ec43386db11a8813438527c91ad6e92b
Kashif <ahmed.kashif1001@gmail.com>
2025-07-21 15:00:09 +0500

    routing fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,7 @@ import Login from "./pages/Login/Login";
 import "./styles/global.scss";
 import User from "./pages/User/User";
 import Product from "./pages/Product/Product";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
@@ -37,38 +33,43 @@ function App() {
     );
   };
 
-  const router = createBrowserRouter([
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          {
+            path: "/",
+            element: <Home />,
+          },
+          {
+            path: "/users",
+            element: <Users />,
+          },
+          {
+            path: "/products",
+            element: <Products />,
+          },
+          {
+            path: "/users/:id",
+            element: <User />,
+          },
+          {
+            path: "/products/:id",
+            element: <Product />,
+          },
+        ],
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
     {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/users",
-          element: <Users />,
-        },
-        {
-          path: "/products",
-          element: <Products />,
-        },
-        {
-          path: "/users/:id",
-          element: <User />,
-        },
-        {
-          path: "/products/:id",
-          element: <Product />,
-        },
-      ],
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-  ]);
+      basename: "/NexDash",
+    }
+  );
 
   return <RouterProvider router={router} />;
 }
