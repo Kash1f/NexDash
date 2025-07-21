1f03d02d67bb9f0f6f2a5c4a8b80e9897c560892
Kashif <ahmed.kashif1001@gmail.com>
2025-07-21 14:44:58 +0500

    Fix: Rename folders/files with consistent casing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import Home from "./pages/home/Home";
+import Home from "./pages/Home/Home";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import Users from "./pages/users/Users";
-import Products from "./pages/products/Products";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
+import Users from "./pages/Users/Users";
+import Products from "./pages/Products/Products";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import Menu from "./components/Menu/Menu";
-import Login from "./pages/login/Login";
+import Login from "./pages/Login/Login";
 import "./styles/global.scss";
-import User from "./pages/user/User";
-import Product from "./pages/product/Product";
+import User from "./pages/User/User";
+import Product from "./pages/Product/Product";
 import {
   QueryClient,
   QueryClientProvider,
