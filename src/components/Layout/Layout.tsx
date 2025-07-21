0fc731fa35d4dcd7d48d6467e3105169acdc7604
Kashif <ahmed.kashif1001@gmail.com>
2025-07-21 14:17:18 +0500

    Temp rename Menu to _temp to fix casing

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-import Menu from "../Menu/Menu";
+import Menu from "../TempMenu/Menu";
 import "./../../styles/global.scss";
 
 const Layout = () => {
